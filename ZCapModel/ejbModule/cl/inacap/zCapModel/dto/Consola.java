89b084e52634260a8303c002960187effb9a3bbf
MatiasZepeda200 <matias.nicolas.zepeda@gmail.com>
2021-05-03 16:27:32 -0400

    Generar interfaz de consolas

--- ZCapModel/ejbModule/cl/inacap/zCapModel/dto/Consola.java
+++ ZCapModel/ejbModule/cl/inacap/zCapModel/dto/Consola.java
@@ -1,5 +1,35 @@
 package cl.inacap.zCapModel.dto;
 
+import java.util.List;
+
 public class Consola {
 
+  private String nombre;
+  private String marca;
+  private int Lanzamiento;
+  private List<Juego> juegos;
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getMarca() {
+	return marca;
+}
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+public int getLanzamiento() {
+	return Lanzamiento;
+}
+public void setLanzamiento(int lanzamiento) {
+	Lanzamiento = lanzamiento;
+}
+public List<Juego> getJuegos() {
+	return juegos;
+}
+public void setJuegos(List<Juego> juegos) {
+	this.juegos = juegos;
+}
 }
